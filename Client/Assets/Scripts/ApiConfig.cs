77efc5fbf6ee314f46c02eba2406bad5db610ed6
Zhan Wang <wz986970406@gmail.com>
2026-03-21 18:36:05 +0800

    update P07

--- Client/Assets/Scripts/ApiConfig.cs
+++ Client/Assets/Scripts/ApiConfig.cs
@@ -10,7 +10,7 @@ public class ApiConfig : ScriptableObject
     [Header("Common Settings")]
     // public string baseDomain = "bj.yi-zhan.top";
     // public string apiKey = "sk-1tbfpLQRbZVig0pa2805B35a9e08426190A7E2Be79E76013";
-    public string baseDomain = "bj.yi-zhan.top";
+    public string baseDomain = "vip.yi-zhan.top";
     public string apiKey = "sk-m1YRJlrBqgG6Y3ZJ2aCb9aFbB07b485e8eC885526a920c29";
 
 
